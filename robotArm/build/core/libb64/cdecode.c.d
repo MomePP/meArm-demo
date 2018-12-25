383058536b19472a8c7abe8f333930f8d5256f3b
MOMELAPTOP\peera <peeranut.31@hotmail.com>
2018-12-25 20:43:54 +0700

    remove build dir

--- robotArm/build/core/libb64/cdecode.c.d
+++
@@ -1,7 +0,0 @@
-c:\Users\peera\Desktop\robotDemo\robotArm\build\core\libb64\cdecode.c.o: \
- C:\Users\peera\Documents\Arduino\hardware\espressif\esp32\cores\esp32\libb64\cdecode.c \
- C:\Users\peera\Documents\Arduino\hardware\espressif\esp32\cores\esp32\libb64\cdecode.h \
- C:\Users\peera\Documents\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/stdint.h \
- C:\Users\peera\Documents\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/machine/_default_types.h \
- C:\Users\peera\Documents\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/sys/features.h \
- C:\Users\peera\Documents\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/sys/_intsup.h
--- robotArm/build/core/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-c:\Users\peera\Desktop\robotDemo\robotArm\build\core\libb64\cencode.c.o: \
- C:\Users\peera\Documents\Arduino\hardware\espressif\esp32\cores\esp32\libb64\cencode.c \
- C:\Users\peera\Documents\Arduino\hardware\espressif\esp32\cores\esp32\libb64\cencode.h
